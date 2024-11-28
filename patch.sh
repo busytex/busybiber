7f97496c4dcf799d804fdae1fd37793c2245cf7a
Vadim Kantorov <vadimkantorov@gmail.com>
2024-11-28 01:17:51 +0100

    ...

---
+++ patch.sh
@@ -0,0 +1,5 @@
+    d_perl_lc_all_uses_name_value_pairs="$undef"
+    d_perl_lc_all_separator="$undef"
+    perl_lc_all_separator=
+    d_perl_lc_all_category_positions_init="$undef"
+    perl_lc_all_category_positions_init=
